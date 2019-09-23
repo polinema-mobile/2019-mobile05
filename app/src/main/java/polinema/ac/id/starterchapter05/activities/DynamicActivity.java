5f4fe606414e95b1b8c343b854d94e8ca5300ca1
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-09-23 16:22:44 +0700

    Dynamic Activity

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -0,0 +1,36 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import polinema.ac.id.starterchapter05.fragments.BlueFragment;
+import polinema.ac.id.starterchapter05.fragments.RedFragment;
+
+import polinema.ac.id.starterchapter05.R;
+
+
+public class DynamicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic);
+    }
+
+    public void handlerClickLoadRedFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment());
+        fragmentTransaction.commit();
+    }
+
+    public void handlerClickLoadBlueFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder, new BlueFragment());
+        fragmentTransaction.commit();
+    }
+
+
+}
