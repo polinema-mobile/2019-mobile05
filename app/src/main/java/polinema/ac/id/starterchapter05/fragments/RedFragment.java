abb6bc5bca1efd9e8b3ec7a218b47800d704bd95
Alan Rizky Wardana <androidmaniac28@gmail.com>
2019-09-23 16:14:13 +0700

    Fix red and blue fragment

--- app/src/main/java/polinema/ac/id/starterchapter05/fragments/RedFragment.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/fragments/RedFragment.java
@@ -2,11 +2,12 @@ package polinema.ac.id.starterchapter05.fragments;
 
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
 
 import polinema.ac.id.starterchapter05.R;
 
